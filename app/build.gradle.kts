d6a98c4c536f6d92c129cadaa21d31adbebfbc70
Edung <edung@example.com>
2026-09-18 17:56:15 -0500

    Remove duplicate android.application plugin alias from app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("com.android.application")
   id("com.google.gms.google-services")
-  alias(libs.plugins.android.application)
+
   alias(libs.plugins.compose.compiler)
   alias(libs.plugins.kotlin.serialization)
 }
